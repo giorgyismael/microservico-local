f61f086b43097cb236e515b139dc93299f5def5f
Giorgy Oliveira <giorgy.oliveira@neogrid.com>
2022-05-29 02:02:51 -0300

    ajuste para tornarser possível buscar o refres das configurações

--- src/main/java/com/gioliveira/configurations/GreetingConfiguration.java
+++ src/main/java/com/gioliveira/configurations/GreetingConfiguration.java
@@ -1,15 +1,19 @@
 package com.gioliveira.configurations;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @Component
-@ConfigurationProperties("greeting-service")
+@NoArgsConstructor
+//Realiza o Refres automatico da configuração que vem do servidor clod
+@RefreshScope 
+//Reliza binding das configraçõs de um determinado serviço automaticamente, seja local, ou cloud
+@ConfigurationProperties("greeting-service") 
 public class GreetingConfiguration {
 	
 	private String greeting;
